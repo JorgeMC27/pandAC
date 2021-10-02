b78e3f884d472ba7ce753b4139ba47975cdfa5db
Vrick <Vrick@172.23.48.46>
2021-10-02 00:17:32 -0500

    Se agrregó clase sitioAyuda

---
+++ src/main/java/com/upemorrally2021/pandac/model/repository/RepositorySitioAyuda.java
@@ -0,0 +1,14 @@
+
+package com.upemorrally2021.pandac.model.repository;
+
+import com.upemorrally2021.pandac.model.entity.SitioAyuda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+/**
+ *
+ * @author Rodrigo C. B.
+ */
+public interface RepositorySitioAyuda extends JpaRepository<SitioAyuda, Long>{
+    
+}
