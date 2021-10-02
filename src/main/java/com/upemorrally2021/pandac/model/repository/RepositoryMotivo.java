55436f62dfed56b3976fc795edbc8fb90cd5c980
cerimice <cerimice@gmail.com>
2021-10-02 00:10:12 -0500

    Se agrego primer repositorio

---
+++ src/main/java/com/upemorrally2021/pandac/model/repository/RepositoryMotivo.java
@@ -0,0 +1,8 @@
+package com.upemorrally2021.pandac.model.repository;
+
+import com.upemorrally2021.pandac.model.entity.Motivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/** @author cerimice **/
+public interface RepositoryMotivo extends JpaRepository<Motivo,Long>{
+}
