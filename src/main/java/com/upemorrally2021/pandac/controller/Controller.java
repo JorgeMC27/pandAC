fbcc33f381e545f4081840a595ab5ef257c869b9
cerimice <cerimice@gmail.com>
2021-10-02 00:22:48 -0500

    Se agrego clase controlador

---
+++ src/main/java/com/upemorrally2021/pandac/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.upemorrally2021.pandac.controller;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/** @author cerimice **/
+public abstract class Controller<Entity,Repository>{
+    
+    @Getter @Autowired private Repository repositorio;
+    
+    protected abstract boolean validar(Entity obj) throws Exception;
+    public abstract Entity guardar(Entity nuevo) throws Exception;
+    
+}
